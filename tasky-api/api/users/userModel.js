854ca5fe94eaa6184e002f9834873a14f6d97dc2
dianaarutcheva <20106796@mail.wit.ie>
2025-12-09 19:13:23 +0000

    Salt and Hash

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -6,5 +7,9 @@ const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
   password: {type: String, required: true }
 });
+
+UserSchema.methods.comparePassword = async function (passw) { 
+    return await bcrypt.compare(passw, this.password); 
+};
 
 export default mongoose.model('User', UserSchema);
