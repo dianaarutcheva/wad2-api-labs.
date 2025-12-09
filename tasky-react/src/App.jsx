6640ddd51aa3ab29cc79a3a2337b8cb755674eed
dianaarutcheva <20106796@mail.wit.ie>
2025-12-09 19:59:49 +0000

    Added auth functions to React app

--- tasky-react/src/App.jsx
+++ tasky-react/src/App.jsx
@@ -1,106 +1,42 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router";
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import LoginPage from "./pages/loginPage";
+import SignupPage from "./pages/signupPage";
+import TasksPage from "./pages/tasksPage";
+import StartPage from "./pages/startPage";
+import ProfilePage from "./pages/profilePage";
 import './App.css';
-import Task from './components/Task';
-import React, { useState, useEffect } from 'react';
-import AddTaskForm from './components/Form';
-import { getTasks, addTask, deleteTask, updateTask } from "./api/tasky-api";
 
-function App() {
-  // Task state
-  const [taskState, setTaskState] = useState({ tasks: [] });
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false
+    },
+  },
+});
 
-  // Load tasks from API when component mounts
-  useEffect(() => {
-    getTasks().then(tasks => {
-      setTaskState({ tasks: tasks });
-    });
-  }, []);
-
-  // Form state for new tasks
-  const [formState, setFormState] = useState({
-    title: "",
-    description: "",
-    deadline: "",
-    priority: "Low"
-  });
-
-  // Toggle task done status
-  const doneHandler = (taskIndex) => {
-    const tasks = [...taskState.tasks];
-    tasks[taskIndex].done = !tasks[taskIndex].done;
-    updateTask(tasks[taskIndex]); // update on API
-    setTaskState({ tasks });
-  }
-
-  // Delete task
-  const deleteHandler = (taskIndex) => {
-    const tasks = [...taskState.tasks];
-    const id = tasks[taskIndex]._id; // use API _id
-    tasks.splice(taskIndex, 1);
-    deleteTask(id); // delete from API
-    setTaskState({ tasks });
-  }
-
-  // Handle changes in the form
-  const formChangeHandler = (event) => {
-    let form = { ...formState };
-
-    switch(event.target.name) {
-      case "title":
-        form.title = event.target.value;
-        break;
-      case "description":
-        form.description = event.target.value;
-        break;
-      case "deadline":
-        form.deadline = event.target.value;
-        break;
-      case "priority":
-        form.priority = event.target.value;
-        break;
-      default:
-        form = formState;
-    }
-
-    setFormState(form);
-  }
-
-  // Handle form submission
-  const formSubmitHandler = async (event) => {
-    event.preventDefault();
-    const tasks = taskState.tasks ? [...taskState.tasks] : [];
-    const form = { ...formState };
-
-    const newTask = await addTask(form); // add task to API
-    tasks.push(newTask);
-    setTaskState({ tasks });
-
-    // Reset form
-    setFormState({ title: "", description: "", deadline: "", priority: "Low" });
-  }
-
+const App = () => {
   return (
-    <div className="container">
-      <h1>Tasky</h1>
-      {taskState.tasks.map((task, index) => (
-        <Task 
-          key={task._id} // use API _id
-          title={task.title}
-          description={task.description}
-          deadline={task.deadline}
-          priority={task.priority}
-          done={task.done}
-          markDone={() => doneHandler(index)}
-          deleteTask={() => deleteHandler(index)}
-        />
-      ))}
-
-      <AddTaskForm 
-        submit={formSubmitHandler} 
-        change={formChangeHandler} 
-        formState={formState} 
-      />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div className="container">
+          <h1>Tasky</h1>
+          <Routes>
+            <Route path="/" element={< StartPage />} />
+            <Route path="/login" element={< LoginPage />} />
+            <Route path="/signup" element={< SignupPage />} />
+            <Route path="/tasks" element={< TasksPage />} />
+            <Route path="/profile" element={< ProfilePage />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
